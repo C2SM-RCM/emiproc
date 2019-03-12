7c69ac1d57bdf55eb7fb845d39e24bbe80e4425b
Jean-Matthieu Haussaire <jean-matthieu.haussaire@empa.ch>
2019-03-12 18:14:51 +0100

    Update and add new Scripts for plotting figures for different models for the CHE GA in march 2019

--- plotting/CHE_Slides/plot_CHIMERE.py
+++ plotting/CHE_Slides/plot_CHIMERE.py
@@ -11,7 +11,6 @@ import matplotlib.colors as colors
 import matplotlib
 from itertools import product
 from datetime import datetime
-import cartopy.io.shapereader as shpreader
 
 
 convert_unit = {
@@ -21,9 +20,9 @@ convert_unit = {
 pole_lon = -170
 pole_lat = 43
 
-path = "/project/s862/CHE/CHE_Europe_output_10s/"
+path = "/project/s862/CHE/CHIMERE_simulation_1w/"
 
-var = "CO2_ALL"
+var = "CO2"
 
 
 
@@ -33,51 +32,46 @@ norm   = matplotlib.colors.BoundaryNorm( bounds, ncolors=256 )
 transform = ccrs.RotatedPole(pole_longitude=pole_lon, pole_latitude=pole_lat)
 
 for i in range(1,10):
-    if i==1:
-        folder = path+"2015010100_0_24/cosmo_output/"
-    else:
-        folder = path+"2015010"+str(i-1)+"18_0_30/cosmo_output/"
-    
+    folder = path
+    date = datetime(2015,1,i)
+    date_str = date.strftime("%Y%m%d")
+    date_disp = date.strftime("%Y-%m-%d %H:00")
+
+    cosmo_1 = nc.Dataset(folder+"out."+date_str+"00.24.nc")
+
+
     for j in range(0,24,1):
         date = datetime(2015,1,i,j)
+    
         date_str = date.strftime("%Y%m%d%H")
-        date_disp = date.strftime("%Y-%m-%d %H:00")
-
-        cosmo_1 = nc.Dataset(folder+"lffd"+date_str+"_co2.nc")
     
         # co2_all= (cosmo_1[var][0,-1,:])
         # co2_bg = (cosmo_1["CO2_BG"][0,-1,:])
         # co2 = co2_bg+co2_all
         
         co2 = (
-            cosmo_1['CO2_BG'][:] + cosmo_1['CO2_ALL'][:] +
-                cosmo_1['CO2_RA'][:] - cosmo_1['CO2_GPP'][:]
+            cosmo_1['CO2'][:]
         )
 
 
-        to_plot = co2[0,-1,:]*convert_unit[var]*pow(10,6)
 
-        cosmo_xlocs = cosmo_1["rlon"][:]
-        cosmo_ylocs= cosmo_1["rlat"][:]
+        to_plot = co2[j,0,:]*pow(10,-3)#*convert_unit[var]
+
+
+
+            # Transform the lon,lat of the ECMWF grid into rotated-pole coordinates
+        all_points = np.array([(x,y) for (x,y) in zip(cosmo_1["lon"][:].flatten(), cosmo_1["lat"][:].flatten())])  #[(x,y) for x in cosmo_1["lon"][:].flatten() for y in cosmo_1["lat"][:].flatten()])
+        grid_points= transform.transform_points(ccrs.PlateCarree(),all_points[:,0],all_points[:,1])
+        cosmo_xlocs = grid_points[:,0]
+        cosmo_xlocs.shape = cosmo_1["lon"][:].shape
+        cosmo_ylocs = grid_points[:,1]
+        cosmo_ylocs.shape = cosmo_1["lat"][:].shape
 
         ax = plt.axes(projection=transform)
 
         # plot borders
         ax.coastlines(resolution="110m")
         ax.add_feature(cartopy.feature.BORDERS)
-
-        # plot major city locations
-        shp_fn = shpreader.natural_earth(resolution='110m', category='cultural', 
-                                         name='populated_places')
-        shp = shpreader.Reader(shp_fn)
-        xy = [pt.coords[0] for pt in shp.geometries()]
-        xy.append( ( 6.845488, 51.431360 ) )#dortmund
-        x, y = zip(*xy)
-        points = transform.transform_points(ccrs.PlateCarree(),np.array(x),np.array(y))
-        x=points[:,0]
-        y=points[:,1]
-        ax.scatter(x,y,25,marker='o',color="c",edgecolors='black',zorder=100)
-
 
 
         vmin=400 #4*pow(10,-4)
@@ -93,14 +87,14 @@ for i in range(1,10):
             mesh = ax.pcolormesh(cosmo_xlocs,cosmo_ylocs,to_plot,vmin=vmin,vmax=vmax)#, norm = norm)# ,vmin=0,vmax=0.8)
             plt.colorbar(mesh)#,ticks=[(6+0.1*i)*pow(10,-4) for i in range(6)])#,norm=norm,boundaries = bounds)
 
-        ax.set_extent([-6,3,0,7],crs=transform)
+        ax.set_extent([-17,21,-11,19.5],crs=transform)
         plt.tight_layout()
         plt.title("CO2 concentrations (in ppm) on %s" %date_disp)
 
         # corners= ccrs.PlateCarree().transform_points(transform,np.array([-17,-17,21,21]),np.array([-11,19.5,-11,19.5]))
         # ax.set_extent([min(corners[:,0]),max(corners[:,0]),min(corners[:,1]),max(corners[:,1])])
 
-        plt.savefig("Figures/Zoom/COSMO/COSMO_zoom_"+date_str+".png")
+        plt.savefig("Figures/CHIMERE/CHIMERE_"+date_str+".png")
         plt.clf()
         # plt.show()
 
--- plotting/CHE_Slides/plot_CHIMERE_zoom.py
+++ plotting/CHE_Slides/plot_CHIMERE_zoom.py
@@ -15,16 +15,15 @@ import cartopy.io.shapereader as shpreader
 
 
 convert_unit = {
-    "co2" : 29/44.,
+    "CO2_ALL" : 29/44.,
     "ch4" : 29/16.}
 
 pole_lon = -170
 pole_lat = 43
 
-folder = "/store/empa/em05/dbrunner/che/icbc/cams_gvri_"
-#cosmo_1 = nc.Dataset("/store/empa/em05/dbrunner/che/icbc/cams_gvri_2015010612.nc")
+path = "/project/s862/CHE/CHIMERE_simulation_1w/"
 
-var = "co2"
+var = "CO2"
 
 
 
@@ -34,31 +33,46 @@ norm   = matplotlib.colors.BoundaryNorm( bounds, ncolors=256 )
 transform = ccrs.RotatedPole(pole_longitude=pole_lon, pole_latitude=pole_lat)
 
 for i in range(1,10):
-    for j in range(0,24,3):
+    folder = path
+    date = datetime(2015,1,i)
+    date_str = date.strftime("%Y%m%d")
+
+    cosmo_1 = nc.Dataset(folder+"out."+date_str+"00.24.nc")
+
+
+    for j in range(0,24,1):
         date = datetime(2015,1,i,j)
-        date_str = date.strftime("%Y%m%d%H")
         date_disp = date.strftime("%Y-%m-%d %H:00")
+    
+        date_str = date.strftime("%Y%m%d%H")
+    
+        # co2_all= (cosmo_1[var][0,-1,:])
+        # co2_bg = (cosmo_1["CO2_BG"][0,-1,:])
+        # co2 = co2_bg+co2_all
+        
+        co2 = (
+            cosmo_1['CO2'][:]
+        )
 
-        cosmo_1 = nc.Dataset(folder+date_str+".nc")
-        co2=(cosmo_1[var][0,-1,:])*pow(10,6)
 
-        to_plot = co2.data*convert_unit[var]
 
-        if True:#i==1 and j==0:
+        to_plot = co2[j,0,:]*pow(10,-3)#*convert_unit[var]
+
+
+
             # Transform the lon,lat of the ECMWF grid into rotated-pole coordinates
-            all_points = np.array([(x,y) for x in cosmo_1["longitude"][:] for y in cosmo_1["latitude"][:]])
-            grid_points= transform.transform_points(ccrs.PlateCarree(),all_points[:,0],all_points[:,1])
-            cosmo_xlocs = grid_points[:,0]
-            cosmo_xlocs.shape = (len(cosmo_1["longitude"][:]),len(cosmo_1["latitude"][:]))
-            cosmo_ylocs = grid_points[:,1]
-            cosmo_ylocs.shape = (len(cosmo_1["longitude"][:]),len(cosmo_1["latitude"][:]))
+        all_points = np.array([(x,y) for (x,y) in zip(cosmo_1["lon"][:].flatten(), cosmo_1["lat"][:].flatten())])  #[(x,y) for x in cosmo_1["lon"][:].flatten() for y in cosmo_1["lat"][:].flatten()])
+        grid_points= transform.transform_points(ccrs.PlateCarree(),all_points[:,0],all_points[:,1])
+        cosmo_xlocs = grid_points[:,0]
+        cosmo_xlocs.shape = cosmo_1["lon"][:].shape
+        cosmo_ylocs = grid_points[:,1]
+        cosmo_ylocs.shape = cosmo_1["lat"][:].shape
 
         ax = plt.axes(projection=transform)
 
         # plot borders
         ax.coastlines(resolution="110m")
         ax.add_feature(cartopy.feature.BORDERS)
-
 
         # plot major city locations
         shp_fn = shpreader.natural_earth(resolution='110m', category='cultural', 
@@ -80,13 +94,12 @@ for i in range(1,10):
         log=False
         if log:
             to_plot_mask = np.ma.masked_where(to_plot<=0, to_plot)
-            mesh = ax.pcolormesh(cosmo_xlocs,cosmo_ylocs,to_plot_mask.T,norm=colors.LogNorm(),vmin=vmin,vmax=vmax)
+            mesh = ax.pcolormesh(cosmo_xlocs,cosmo_ylocs,to_plot_mask,norm=colors.LogNorm(),vmin=vmin,vmax=vmax)
             plt.colorbar(mesh,ticks=[(6+0.1*i)*pow(10,-4) for i in range(6)])
         else:   
             #to_plot_mask = np.ma.masked_where(np.logical_not(np.isfinite(to_plot)), to_plot)
-            mesh = ax.pcolormesh(cosmo_xlocs,cosmo_ylocs,to_plot.T,vmin=vmin,vmax=vmax)#, norm = norm)# ,vmin=0,vmax=0.8)
+            mesh = ax.pcolormesh(cosmo_xlocs,cosmo_ylocs,to_plot,vmin=vmin,vmax=vmax)#, norm = norm)# ,vmin=0,vmax=0.8)
             plt.colorbar(mesh)#,ticks=[(6+0.1*i)*pow(10,-4) for i in range(6)])#,norm=norm,boundaries = bounds)
-
 
         ax.set_extent([-6,3,0,7],crs=transform)
         plt.tight_layout()
@@ -95,8 +108,8 @@ for i in range(1,10):
         # corners= ccrs.PlateCarree().transform_points(transform,np.array([-17,-17,21,21]),np.array([-11,19.5,-11,19.5]))
         # ax.set_extent([min(corners[:,0]),max(corners[:,0]),min(corners[:,1]),max(corners[:,1])])
 
-        plt.savefig("Figures/Zoom/CAMS/cams_zoom_"+date_str+".png")
+        plt.savefig("Figures/Zoom/CHIMERE/CHIMERE_"+date_str+".png")
         plt.clf()
-        #plt.show()
+        # plt.show()
 
    
