c8ac9124ec6160b18719de1ebff37adb8f0e979c
Haussaire JM <jean-matthieu.haussaire@empa.ch>
2019-06-19 19:57:21 +0200

    Clean up and simplify the main_tno to make a new, cleaner one

---
+++ config_tno.py
@@ -0,0 +1,39 @@
+# TNO inventory
+tnofile = "/input/TNOMACC/TNO_GHGco/Future_years_emissions/TNO_GHGco_v1_1_CIRCE_BAU_year2030.nc"
+
+tno_xmin = -30. 
+tno_xmax = 60.
+tno_ymin = 30.
+tno_ymax = 72. 
+tno_dx = 1/10.
+tno_dy = 1/20.
+
+cat_kind="NFR"
+
+tno_cat = [ "A", "B", "C", "D", "E", "F1","F2","F3",
+        "G", "H", "I", "J", "K","L" ]
+
+# COSMO domain
+xmin = -17
+ymin = -11
+nx = 760
+ny = 610
+dx = 0.05
+dy = 0.05
+pollon = -170.0
+pollat = 43.0
+
+offline=False
+if offline:
+    xmin -= 2*dx
+    ymin -= 2*dy
+    nx += 4
+    ny += 4
+
+
+species = ['co2_ff']
+output_cat = [ "A", "B"]
+
+output_path = "./testdata/oae_paper/"
+output_name = "tno.nc"
+
