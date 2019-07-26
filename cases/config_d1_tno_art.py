0031b591193c68b7694cca17114bbf46594820ba
Gerrit Kuhlmann <gerrit.kuhlmann@empa.ch>
2019-07-26 12:13:23 +0200

    Add TNO and Swiss inventory config files for COSMO-ART

---
+++ cases/config_d1_tno_art.py
@@ -0,0 +1,104 @@
+# "constant" paths and values for TNO, regular lat/lon
+# for MeteoTest Swiss inventory, use calculated regular domain in the code
+
+import os
+import time
+
+from epro.grids import COSMOGrid, TNOGrid
+
+# inventory
+inventory = 'TNO'
+
+# model either "cosmo-art" or "cosmo-ghg" (affects the output units)
+model = 'cosmo-art'
+
+# input filename
+input_path = "/input/TNOMACC/CAMS-REG-AP_v2_2/CAMS-REG-AP_v2_2_1_emissions_year2015.nc"
+
+# input grid
+input_grid = TNOGrid(input_path)
+
+# input species
+species = ['co', 'nox', 'nmvoc', 'so2', 'nh3', 'pm10', 'pm2_5']
+
+# input categories
+categories = ["A", "B", "C", "D", "E", "F1","F2","F3","F4",
+              "G", "H", "I", "J", "K", "L" ]
+
+# mapping from input to output species (input is used for missing keys)
+in2out_species = {
+    'co':    'CO',
+    'nox':   'NOX',
+    'nmvoc': 'NMVOC',
+    'so2':   'SO2',
+    'nh3':   'NH3',
+    'pm10':  'PM10',
+    'pm2_5': 'PM25',
+
+}
+
+# mapping from input to output categories (input is used for missing keys)
+in2out_category = {
+    'F1': 'F',
+    'F2': 'F',
+    'F3': 'F',
+    'F4': 'F'
+}
+
+# output variables are written in the following format using species and
+# category after applying the mapping
+varname_format = '{species}_{category}_{source_type}'
+
+# online or offline emissions (online emissions have a grid with 2-cell
+# boundary)
+offline = False
+
+# output path and filename
+if offline:
+    output_path = os.path.join('oae-art-example', 'offline', 'tno')
+else:
+    output_path = os.path.join('oae-art-example', 'online', 'tno')
+
+output_name = 'tno-art.nc'
+
+
+# Output grid is European domain (rotated pole coordinates)
+xmin = -16.08
+ymin =  -9.54
+nx = 192
+ny = 164
+
+if offline:
+    xmin -= 2 * dx
+    ymin -= 2 * dy
+    nx += 4
+    ny += 4
+
+cosmo_grid = COSMOGrid(
+    nx=nx,
+    ny=ny,
+    dx=0.12,
+    dy=0.12,
+    xmin=xmin,
+    ymin=ymin,
+    pollon=-170.0,
+    pollat=43.0,
+)
+
+# resolution of shapefile used for country mask
+shpfile_resolution = "110m"
+
+# number of processes
+nprocs = 16
+
+# metadata added as global attributes to netCDF output file
+nc_metadata = {
+    "DESCRIPTION": "Gridded annual emissions",
+    "DATAORIGIN": "TNO-CAMS",
+    "CREATOR": "Qing Mu and Gerrit Kuhlmann",
+    "EMAIL": "gerrit.kuhlmann@empa.ch",
+    "AFFILIATION": "Empa Duebendorf, Switzerland",
+    "DATE CREATED": time.ctime(time.time()),
+}
+
+
