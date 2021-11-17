77deb8bc889ad71e0df3b86f78d914aad55b167d
efmkoene <erikkoene@hotmail.com>
2021-11-17 10:08:29 +0100

    refer to correct case file

--- tests/performance_at_rlon.py
+++ tests/performance_at_rlon.py
@@ -5,7 +5,7 @@ import amrs.nc
 def test_performance_at_rlon():
     # Execute processing case which has rlon==0. Hard-coded (in shell) to save effort of sending in all correct arguments otherwise...
     out_filename = "outgrid.nc"
-    subprocess.run(["python -m ../emiproc grid --case-file tno_test.py --output_path output-path . --output-name {}".format(out_filename)])
+    subprocess.run(["python -m ../emiproc grid --case-file tno_rlon_test.py --output_path output-path . --output-name {}".format(out_filename)])
     
     # Reference file in tests/ folder
     out_filename_ref = "tno_ref.nc"
