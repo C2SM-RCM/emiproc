6c9b8c3464a9d07a86d55f9ddb1bd593b42e5162
Constantin, Lionel <lionel.constantin@empa.ch>
2023-01-31 15:19:54 +0100

    clear for new pygg version

--- scripts/zh_2_gral.py
+++ scripts/zh_2_gral.py
@@ -14,30 +14,11 @@ file = Path("/store/empa/em05/mapluft/mapLuft_2020_v2021.gdb")
 zh_inv = MapLuftZurich(file, ['CO2'], convert_lines_to_polygons=False)
 zh_inv.emission_infos = ZURICH_SOURCES
 zh_inv 
-#%%
-
-import importlib
-import emiproc.exports.gral
-importlib.reload(emiproc.exports.gral)
+#%% Read the gral grid from a generated geb
 from emiproc.exports.gral import export_to_gral
-import pygg.grids
-importlib.reload(pygg.grids)
 from pygg.grids import GralGrid
-grid = GralGrid(
-        dz=2.0,
-        stretch=1.0,
-        nx=1368,
-        ny=1296,
-        nslice=9,
-        sourcegroups="",
-        xmin=2676060,
-        xmax=2689740,
-        ymin=1241540,
-        ymax=1254500,
-        latitude=47,
-        crs=zh_inv.crs,
-    )
-grid.building_heights = np.zeros((grid.nx, grid.ny))
+grid = GralGrid.from_gral_rundir("/scratch/snx3000/lconstan/gramm_gral2/")
+
 #%% Crop the invenotry over the gral grid 
 from emiproc.inventories.utils import crop_with_shape
 from emiproc.inventories.utils import get_total_emissions
