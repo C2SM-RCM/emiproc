9269f3453595f835dd3973eaa4ccbbd3fe60e06b
coli <lionel.constantin@empa.ch>
2023-04-18 17:07:00 +0200

    yml profiles

--- emiproc/profiles/temporal_profiles.py
+++ emiproc/profiles/temporal_profiles.py
@@ -1,11 +1,14 @@
 """Temporal profiles."""
-
+from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
+import logging
+from os import PathLike
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
-
+import yaml
 
 # Constants
 N_HOUR_DAY = 24
@@ -35,6 +38,9 @@ class TemporalProfile:
             raise ValueError(
                 f"{len(self.ratios)=} does not match profile's {self.size=}."
             )
+
+        if isinstance(self.ratios, list):
+            self.ratios = np.array(self.ratios)
 
         # Make sure the ratios sum up to 1
         if not np.isclose(self.ratios.sum(), 1.0):
@@ -156,3 +162,184 @@ def profile_to_scaling_factors(
 
     # Return the scaling factors
     return scaling_factors
+
+
+AnyTimeProfile = (
+    DailyProfile
+    | WeeklyProfile
+    | MounthsProfile
+    | HourOfYearProfile
+    | HourOfLeapYearProfile
+)
+
+
+def from_csv(
+    file: PathLike,
+) -> dict[str | tuple[str, str] : AnyTimeProfile]:
+    """Create the profile from a csv file.
+
+    Based on the file, guess the correct profile
+    to create
+    """
+    logger = logging.getLogger("emiproc.profiles.from_csv")
+
+    file = Path(file)
+
+    df = pd.read_csv(file)
+
+    if "Category" in df.columns:
+        cat_header = "Category"
+    else:
+        raise ValueError(f"Cannot find 'Category' header in {file=}")
+
+    if "Substance" in df.columns:
+        sub_header = "Substance"
+    else:
+        sub_header = None
+        logger.warning(
+            f"Cannot find 'Substance' header in {file=}."
+            "All substances will be treated the same way."
+        )
+
+    logger.error(f"{df.columns=}")
+
+    if "Mon" in df.columns:
+        # Weekly profile with 3 letters identification
+        data_columns = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        profile = WeeklyProfile
+    elif "Monday" in df.columns:
+        # Weekly profile with full name identification
+        data_columns = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ]
+        profile = WeeklyProfile
+    elif "Jan" in df.columns:
+        # Yearly profile with 3 letters identification
+        data_columns = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ]
+        profile = MounthsProfile
+    elif "January" in df.columns:
+        # Yearly profile with full name identification
+        data_columns = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ]
+        profile = MounthsProfile
+    elif (
+        "0" not in df.columns
+        and "1" in df.columns
+        and "24" in df.columns
+        and "25" not in df.columns
+    ):
+        # Daily profile
+        data_columns = [str(i) for i in range(1, 25)]
+        profile = DailyProfile
+    else:
+        raise NotImplementedError(f"Cannot guess the profile from {file=}")
+
+    # Check that all the data columns are in the file
+    for col in data_columns:
+        if not col in df.columns:
+            raise ValueError(
+                f"Cannot find {col=} in {file=}, required to build {profile=}"
+            )
+
+    # Read all rows to generate the profiles
+    profiles = {}
+    for _, row in df.iterrows():
+        cat = row[cat_header]
+        if sub_header is not None:
+            sub = row[sub_header]
+            key = (cat, sub)
+        else:
+            key = cat
+        ratios = np.array([row[col] for col in data_columns])
+
+        # Check if scaling factors are given instead of ratios
+        if np.isclose(ratios.sum(), len(ratios)):
+            ratios = ratios / ratios.sum()
+        elif not np.isclose(ratios.sum(), 1.0):
+            pass  # Already ratios
+        else:
+            raise ValueError(
+                f"Cannot guess if {ratios=} are ratios or scaling factors for {row=} in {file=}."
+            )
+        profiles[key] = profile(ratios=ratios)
+
+    return profiles
+
+
+
+def from_yaml(yaml_file: PathLike) -> list[AnyTimeProfile]:
+    """Read a yml file containing a temporal profile.
+    
+    Only one temporal profile is currently accepted in the yaml definition.
+    """
+    logger = logging.getLogger("emiproc.profiles.from_yaml")
+    yaml_file = Path(yaml_file)
+
+    with open(yaml_file, "r") as f:
+        data = yaml.safe_load(f)
+    
+    if data is None:
+        logger.warning(f"Empty yaml file {yaml_file=}")
+        return []
+
+    profiles = []
+
+    # Create possible aliases for the names 
+    profiles_mapping: dict[AnyTimeProfile, list[str]] = {
+        DailyProfile: ["diurn", "daily", "day"],
+        WeeklyProfile: ["weekly", "week"],
+        MounthsProfile: ["season", "year", "monthly", "month"],
+    }
+    profile_of_key = {key: profile for profile, keys in profiles_mapping.items() for key in keys}
+
+    _types_added = []
+    # Check that the yaml does not contain any unkown key 
+    for key in data.keys():
+        if not any(key in profile_names for profile_names in profiles_mapping.values()):
+            logger.warning(f"Unknown key {key=} in {yaml_file=}")
+            continue 
+        profile_class = profile_of_key[key]
+        # Check that a profile of that type was not already added
+        if profile_class in _types_added:
+            raise ValueError(f"Cannot add {key=} to {yaml_file=} as a {profile_class=} was already added.")
+        # add the profile 
+        ratios = data[key]
+        # Check the ratio 
+        if not np.isclose(np.sum(ratios), 1.0):
+            raise ValueError(f"{ratios=} in {yaml_file=} do not sum to 1 but {np.sum(ratios)}.")
+        profiles.append(profile_class(ratios=ratios))
+        
+    if len(profiles) == 0:
+        logger.warning(f"No profile found in {yaml_file=}")
+    return profiles
--- tests/test_temporal_profiles.py
+++ tests/test_temporal_profiles.py
@@ -1,5 +1,5 @@
 """Test some basic features of the inventory profiles."""
-#%%
+# %%
 import xarray as xr
 from pathlib import Path
 import numpy as np
@@ -14,7 +14,9 @@ from emiproc.profiles.temporal_profiles import (
     DailyProfile,
     WeeklyProfile,
     MounthsProfile,
-    create_time_serie
+    create_time_serie,
+    from_csv,
+    from_yaml
 )
 from emiproc.profiles.operators import (
     weighted_combination,
@@ -22,10 +24,10 @@ from emiproc.profiles.operators import (
     get_weights_of_gdf_profiles,
 )
 
-#%% test 
+# %% test
 
 DailyProfile()
-#%% Create test profiles
+# %% Create test profiles
 test_profiles = [
     DailyProfile(),
     WeeklyProfile(),
@@ -33,10 +35,10 @@ test_profiles = [
     TemporalProfile(
         size=3,
         ratios=np.array([0.3, 0.2, 0.5]),
-    )
+    ),
 ]
 
-#%%
+# %%
 ratios = create_time_serie(
     start_time="2020-01-01",
     end_time="2020-01-31",
@@ -44,3 +46,46 @@ ratios = create_time_serie(
 )
 ratios
 
+# TODO: move the following to examples
+# %%
+profiles_month_in_year = from_csv(
+    r"C:\Users\coli\Documents\emiproc\files\profiles\copernicus\month_in_year.csv"
+)
+profiles_day_in_week = from_csv(
+    r"C:\Users\coli\Documents\emiproc\files\profiles\copernicus\day_in_week.csv"
+)
+profiles_hour_in_day = from_csv(
+    r"C:\Users\coli\Documents\emiproc\files\profiles\copernicus\hour_in_day.csv"
+)
+# %% plot the profiles
+import matplotlib.pyplot as plt
+import pandas as pd
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+for name, profile in profiles_month_in_year.items():
+    ax.plot(profile.ratios, label=name)
+ax.legend()
+# %%
+tss = {}
+for categorie in profiles_month_in_year.keys():
+    tss[categorie] = create_time_serie(
+        start_time="2020-01-01",
+        end_time="2020-02-28",
+        profiles=[
+            profiles_month_in_year[categorie],
+            profiles_day_in_week[categorie],
+            profiles_hour_in_day[categorie],
+        ],
+    )
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+for name, ts in tss.items():
+    ax.plot(ts, label=name)
+ax.legend()
+# %%
+yaml_dir = Path(r"C:\Users\coli\Documents\emiproc\files\profiles\yamls")
+
+for yml_file in yaml_dir.glob("*.yaml"):
+    from_yaml(yml_file)
+
+# %%
