357cc46f262dab4e2d444baed8a8efc75c57fe19
coli <lionel.constantin@empa.ch>
2023-05-08 11:11:01 +0200

    formatting and cleaning

--- emiproc/inventories/gfas.py
+++ emiproc/inventories/gfas.py
@@ -1,48 +1,51 @@
 from os import PathLike
-from pathlib import Path
+
 import xarray as xr
 import numpy as np
-import re
 import geopandas as gpd
-import pyogrio
 
-from emiproc.grids import WGS84, RegularGrid, LatLonNcGrid
+from emiproc.grids import WGS84, RegularGrid
 from emiproc.inventories import Inventory
 from emiproc.utilities import SEC_PER_YR
 
+
 class GFAS_Inventory(Inventory):
     """The GFAS inventory.
-    
-    `CAMS global biomass burning emissions based on fire radiative power  <https://ads.atmosphere.copernicus.eu/cdsapp#!/dataset/cams-global-fire-emissions-gfas>`_
-    
+
+    `CAMS global biomass burning emissions based on fire radiative power 
+    <https://ads.atmosphere.copernicus.eu/cdsapp#!/dataset/cams-global-fire-emissions-gfas>`_
+
     GFAS has only grid cell sources.
     """
+
     grid: RegularGrid
 
     def __init__(self, nc_file: PathLike):
         """Create a GFAS.
         The GFAS directory contains gridded data of forest fires (co2 fluxes)
-                
+
         """
         super().__init__()
         ds = xr.open_dataset(nc_file)
-        categories = ['co2fire', 'mami']
+        categories = ["co2fire", "mami"]
 
         substances_mapping = {
             "co2fire": "CO2",
         }
 
         # Take the mean of the fire emissions over the time period submitted
-        if 'time' in ds.keys():
-            ds = ds.mean('time', keep_attrs=True)
-        
-        self.grid = RegularGrid(ds.longitude[0].values, 
-                                ds.longitude[-1].values, 
-                                ds.latitude[0].values, # Sorted from high to low
-                                ds.latitude[-1].values,
-                                len(ds.longitude),
-                                len(ds.latitude))
-                
+        if "time" in ds.keys():
+            ds = ds.mean("time", keep_attrs=True)
+
+        self.grid = RegularGrid(
+            ds.longitude[0].values,
+            ds.longitude[-1].values,
+            ds.latitude[0].values,  # Sorted from high to low
+            ds.latitude[-1].values,
+            len(ds.longitude),
+            len(ds.latitude),
+        )
+
         polys = self.grid.cells_as_polylist
 
         # Index in the polygon list (from the gdf) (index start at 1)
@@ -68,4 +71,4 @@ class GFAS_Inventory(Inventory):
         self.cell_areas = self.grid.cell_areas
 
         # -- Convert to kg/yr
-        self.gdf[list(mapping)] *= SEC_PER_YR * self.cell_areas[:, np.newaxis]  
+        self.gdf[list(mapping)] *= SEC_PER_YR * self.cell_areas[:, np.newaxis]
