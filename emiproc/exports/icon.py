039418422c6ab2ae5761566c5939d151938c5526
coli <lionel.constantin@empa.ch>
2023-07-17 17:20:16 +0200

    addding constant profile for icon and fixing

--- emiproc/exports/icon.py
+++ emiproc/exports/icon.py
@@ -35,13 +35,25 @@ class TemporalProfilesTypes(Enum):
     THREE_CYCLES = auto()
 
 
+def get_constant_time_profile() -> list[TemporalProfile]:
+    """Get a constant time profile for ICON-OEM.
+    
+    Emits the same at every time. 
+    
+    """
+    return [
+        DailyProfile(), # Hour of day
+        WeeklyProfile(), # Day of week
+        MounthsProfile(), # Month of year
+    ]
+
 def export_icon_oem(
     inv: Inventory,
     icon_grid_file: PathLike,
     output_dir: PathLike,
     group_dict: dict[str, list[str]] = {},
     country_resolution: str = "10m",
-    temporal_profiles_type: TemporalProfilesTypes = TemporalProfilesTypes.HOUR_OF_YEAR,
+    temporal_profiles_type: TemporalProfilesTypes = TemporalProfilesTypes.THREE_CYCLES,
     year: int | None = None,
     nc_attributes: dict[str, str] = DEFAULT_NC_ATTRIBUTES,
     substances: list[str] | None = None,
