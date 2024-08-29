f0e6f625a6e259475165e36eb061ca2369b1fd7a
coli <lionel.constantin@empa.ch>
2024-08-29 16:37:22 +0200

    added edgar v8

--- emiproc/inventories/edgar.py
+++ emiproc/inventories/edgar.py
@@ -1,3 +1,4 @@
+import logging
 from os import PathLike
 from pathlib import Path
 import xarray as xr
@@ -6,27 +7,108 @@ import re
 import geopandas as gpd
 import pyogrio
 
-from emiproc.grids import WGS84, EDGARGrid
+from emiproc.grids import WGS84, EDGARGrid, RegularGrid
 from emiproc.inventories import Inventory
 from emiproc.utilities import SEC_PER_YR
 
-class EDGAR_Inventory(Inventory):
+
+class EDGARv8(Inventory):
     """The EDGAR inventory.
-    
+
     `Emissions Database for Global Atmospheric Research <https://edgar.jrc.ec.europa.eu/>`_
-    
+
     EDGAR has only grid cell sources.
     """
+
     grid: EDGARGrid
 
-    def __init__(self, nc_file_pattern: PathLike, grid_shapefile: PathLike | None = None) -> None:
+    def __init__(
+        self,
+        nc_file_pattern: PathLike,
+        year: int | None = None,
+    ) -> None:
+        """Create a EDGAR_Inventory.
+
+        :arg nc_file_pattern: Pattern of files, e.g "path/to/edgarfiles/v8.0_FT2021_SF6_*_PRU.0.1x0.1.nc"
+
+        """
+        super().__init__()
+
+        nc_file_pattern = Path(nc_file_pattern)
+        logger = logging.getLogger(__name__)
+
+        list_filepaths = list(nc_file_pattern.parent.glob(nc_file_pattern.name))
+
+        columns = {}
+
+        file_for_grid = None
+
+        for filepath in list_filepaths:
+            ds = xr.open_dataset(filepath)
+            if "fluxes" not in ds:
+                logger.warning(
+                    f"File {filepath} does not contain 'fluxes' variable. Skipping."
+                )
+                continue
+            da = ds["fluxes"]
+            substance = da.attrs["substance"]
+            category = da.attrs["long_name"]
+            file_year = int(da.attrs["year"])
+            if year is None:
+                year = file_year
+            if file_year != year:
+                logger.warning(
+                    f"File {filepath} has year {file_year}, expected {year}. Skipping."
+                )
+                continue
+            units = da.attrs["units"]
+            assert units == "kg m-2 s-1", f"Units are {units}, expected kg m-2 s-1"
+
+            columns[(category, substance)] = da.data.T.reshape(-1)
+
+            if file_for_grid is None:
+                file_for_grid = ds
+
+        self.grid = RegularGrid.from_centers(
+            ds["lon"].data, ds["lat"].data, name="EDGARv8_grid"
+        )
+        cell_areas = self.grid.cell_areas
+
+        # Swtich longitude from 0/360 to -180/180
+        self.gdf = gpd.GeoDataFrame(
+            data={
+                # Convert to kg/yr
+                col: value * SEC_PER_YR * cell_areas
+                for col, value in columns.items()
+            },
+            geometry=self.grid.gdf.geometry,
+            crs=WGS84,
+        )
+
+        # Empty shaped sources
+        self.gdfs = {}
+
+
+class EDGAR_Inventory(Inventory):
+    """The EDGAR inventory.
+
+    `Emissions Database for Global Atmospheric Research <https://edgar.jrc.ec.europa.eu/>`_
+
+    EDGAR has only grid cell sources.
+    """
+
+    grid: EDGARGrid
+
+    def __init__(
+        self, nc_file_pattern: PathLike, grid_shapefile: PathLike | None = None
+    ) -> None:
         """Create a EDGAR_Inventory.
 
         The EDGAR directory that contains the original datasets must be structured as ./substance/categories/dataset,
         e.g., ./SF6/NFE/v7.0_FT2021_SF6_*_NFE.0.1x0.1.nc and ./SF6/PRU/v7.0_FT2021_SF6_*_PRU.0.1x0.1.nc
-        
+
         :arg nc_file_pattern: Pattern of files, e.g "EDGAR/SF6/PRU/v7.0_FT2021_SF6_*_PRU.0.1x0.1.nc"
-        
+
         """
         super().__init__()
 
@@ -40,7 +122,10 @@ class EDGAR_Inventory(Inventory):
         substance = list_filepaths[0].parent.parent.stem
 
         substances_mapping = {}
-        categories = [re.search(r'{}_(.+?)_(.+?)\.'.format(substance), filepath.name)[2] for filepath in list_filepaths] 
+        categories = [
+            re.search(r"{}_(.+?)_(.+?)\.".format(substance), filepath.name)[2]
+            for filepath in list_filepaths
+        ]
 
         for i, ds in enumerate(list_ds):
             old_varname = list(ds.data_vars.keys())[0]
@@ -52,7 +137,7 @@ class EDGAR_Inventory(Inventory):
 
         # Swtich longitude from 0/360 to -180/180
         attributes = ds.lon.attrs
-        ds = ds.assign(lon=(ds.lon + 180) % 360 - 180).sortby('lon')
+        ds = ds.assign(lon=(ds.lon + 180) % 360 - 180).sortby("lon")
         ds.lon.attrs = attributes
 
         self.grid = EDGARGrid(list_filepaths[0])
@@ -88,4 +173,4 @@ class EDGAR_Inventory(Inventory):
         self.cell_areas = self.grid.cell_areas
 
         # -- Convert to kg/yr
-        self.gdf[list(mapping)] *= SEC_PER_YR * self.cell_areas[:, np.newaxis]  
+        self.gdf[list(mapping)] *= SEC_PER_YR * self.cell_areas[:, np.newaxis]
